b07fc6f4fc12f94c3467b0796745fe3982b121e6
Dione Martins <dbarrama@everis.com>
2021-02-07 19:00:15 -0300

    Melhorias na interface e acesso ao bluetooth

--- app/src/main/java/com/example/listapresencabluetooth/MainActivity.java
+++ app/src/main/java/com/example/listapresencabluetooth/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.listapresencabluetooth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    BluetoothAdapter bluetooth_adapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bluetooth_adapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    public void atualizar(View view) {
+        if (bluetooth_adapter == null) {
+            Toast.makeText(this, this.getString(R.string.mg_erro_bluetooth), Toast.LENGTH_SHORT).show();
+        } else if (!bluetooth_adapter.isEnabled()) {
+            Intent ativa_bluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(ativa_bluetooth, 1);
+        } else {
+            //TODO
+        }
     }
 }
