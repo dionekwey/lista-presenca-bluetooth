83b3126bd7b892b1462b1c045ac886e47599e856
Dione Martins <dbarrama@everis.com>
2021-02-10 23:22:55 -0300

    Buscar dispositivos próximos

--- app/src/main/java/com/example/listapresencabluetooth/MainActivity.java
+++ app/src/main/java/com/example/listapresencabluetooth/MainActivity.java
@@ -6,29 +6,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import java.util.Set;
-
 public class MainActivity extends AppCompatActivity {
 
     private static final int SOLICITA_ATIVACAO_BT = 1;
 
-    private BluetoothAdapter bluetooth_adapter = null;
-    private ListView lista_dispositivos = null;
+    private BluetoothAdapter mBluetoothAdapter = null;
+    private ListView mListaDispositivos = null;
+    private BroadcastReceiver mReceiver = null;
+    private ArrayAdapter<String> mLista = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bluetooth_adapter = BluetoothAdapter.getDefaultAdapter();
-        lista_dispositivos = findViewById(R.id.lista_dispositivos);
+        mListaDispositivos = findViewById(R.id.lista_dispositivos);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        mLista = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        mListaDispositivos.setAdapter(mLista);
+
+        mReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    String deviceName = device.getName();
+                    String deviceAddress = device.getAddress();
+
+                    mLista.add(deviceName + " [" + deviceAddress + "]");
+                }
+            }
+        };
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+
+        /*
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            showMessage("ACCESS_COARSE_LOCATION não permitido");
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
+            showMessage("ACCESS_FINE_LOCATION não permitido");
+        }
+        */
     }
 
     @Override
@@ -42,16 +75,23 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void atualizar(View view) {
-        limparLista();
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        //unregisterReceiver(mReceiver);
+    }
 
-        if (bluetooth_adapter == null) {
+    public void atualizar(View view) {
+        mLista.clear();
+
+        if (mBluetoothAdapter == null) {
             showMessage(this.getString(R.string.mg_erro_bluetooth));
-        } else if (!bluetooth_adapter.isEnabled()) {
+        } else if (mBluetoothAdapter.isEnabled()) {
+            listarDispositivos();
+        } else {
             Intent ativa_bluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(ativa_bluetooth, SOLICITA_ATIVACAO_BT);
-        } else {
-            listarDispositivos();
         }
     }
 
@@ -59,23 +99,24 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
-    private void limparLista() {
-        lista_dispositivos.setAdapter(null);
-    }
-
     private void listarDispositivos() {
-        ArrayAdapter<String> lista = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-        Set<BluetoothDevice> dispositivos_disponiveis = bluetooth_adapter.getBondedDevices();
+        mBluetoothAdapter.startDiscovery();
 
-        if (dispositivos_disponiveis.size() > 0) {
-            for (BluetoothDevice dispositivo : dispositivos_disponiveis) {
-                String nome = dispositivo.getName();
-                String address = dispositivo.getAddress();
-
-                lista.add(nome + " [" + address + "]");
-            }
-
-            lista_dispositivos.setAdapter(lista);
+        if (mBluetoothAdapter.isDiscovering()) {
+            showMessage("Buscando...");
         }
+
+        /*
+        Set<BluetoothDevice> dispositivosPareados = mBluetoothAdapter.getBondedDevices();
+
+        if (dispositivosPareados.size() > 0) {
+            for (BluetoothDevice dispositivo : dispositivosPareados) {
+                String deviceName = dispositivo.getName();
+                String deviceAddress = dispositivo.getAddress();
+
+                mLista.add(deviceName + " [" + deviceAddress + "]");
+            }
+        }
+         */
     }
 }
